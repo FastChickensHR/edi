3d61cff24d01b788eec4f802812da6730a48b2db
mandymoorefan <markbeeb@gmail.com>
2025-03-13 16:14:28 -0700

    feat(payer): add initial class and builder with tests

---
+++ src/main/java/com/fastChickensHR/edi/x834/loop1000B/Payer.java
@@ -0,0 +1,88 @@
+package com.fastChickensHR.edi.x834.loop1000B;
+
+import com.fastChickensHR.edi.x834.common.Segment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+public abstract class Payer extends Segment {
+    public static final String SEGMENT_ID = "N1";
+    public static final String DEFAULT_ENTITY_IDENTIFIER_CODE = "IN";
+    public static final String DEFAULT_IDENTIFICATION_CODE_QUALIFIER = "FI";
+
+    private final String n101;
+    private final String n102;
+    private final String n103;
+    private final String n104;
+
+    protected Payer(Builder builder) throws ValidationException {
+        this.n101 = builder.n101;
+        this.n102 = builder.n102;
+        this.n103 = builder.n103;
+        this.n104 = builder.n104;
+    }
+
+    @Override
+    public String getSegmentIdentifier() {
+        return SEGMENT_ID;
+    }
+
+    @Override
+    public String[] getElementValues() {
+        return new String[]{n101, n102, n103, n104};
+    }
+
+    public String getEntityIdentifierCode() {
+        return getN101();
+    }
+
+    public String getPlanSponsorName() {
+        return getN102();
+    }
+
+    public String getIdentificationCodeQualifier() {
+        return getN103();
+    }
+
+    public String getSponsorIdentifier() {
+        return getN104();
+    }
+
+    @Setter
+    @Accessors(chain = true)
+    public static class Builder {
+        private String n101 = DEFAULT_ENTITY_IDENTIFIER_CODE;
+        private String n102;
+        private String n103 = DEFAULT_IDENTIFICATION_CODE_QUALIFIER;
+        private String n104;
+
+        public Builder setEntityIdentifierCode(String value) {
+            return setN101(value);
+        }
+
+        public Builder setPlanSponsorName(String value) {
+            return setN102(value);
+        }
+
+        public Builder setIdentificationCodeQualifier(String value) {
+            return setN103(value);
+        }
+
+        public Builder setSponsorIdentifier(String value) {
+            return setN104(value);
+        }
+
+        public Payer build() throws ValidationException {
+            return new Payer.PayerImpl(this);
+        }
+    }
+
+    // Concrete implementation class that can be instantiated
+    private static class PayerImpl extends Payer {
+        private PayerImpl(Builder builder) throws ValidationException {
+            super(builder);
+        }
+    }
+}
---
+++ src/test/java/com/fastChickensHR/edi/x834/loop1000B/PayerTest.java
@@ -0,0 +1,59 @@
+package com.fastChickensHR.edi.x834.loop1000B;
+
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import com.fastChickensHR.edi.x834.common.x834Context;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PayerTest {
+    x834Context context = new x834Context();
+    String entityIdentifierCode = "T3";
+    String planSponsorName = "fake plan sponsor name";
+    String identificationCodeQualifier = "FLL";
+    String sponsorIdentifier = "FPO";
+
+    @Test
+    public void testGetSegmentIdentifierReturnsExpectedValue() throws ValidationException {
+
+
+        Payer segment = new Payer.Builder()
+                .setN102(planSponsorName)
+                .setN104(sponsorIdentifier)
+                .build();
+        segment.setContext(context);
+
+        assertEquals("N1", segment.getSegmentIdentifier(), "Expected segment identifier should be 'N1'");
+        assertEquals("N1*IN*fake plan sponsor name*FI*FPO~", segment.render().trim(), "The segment is not formatted correctly.");
+    }
+
+    @Test
+    public void testSettingSpecNamesGettingDomainNames() throws ValidationException {
+        Payer segment = new Payer.Builder()
+                .setN101(entityIdentifierCode)
+                .setN102(planSponsorName)
+                .setN103(identificationCodeQualifier)
+                .setN104(sponsorIdentifier)
+                .build();
+
+        assertEquals(entityIdentifierCode, segment.getEntityIdentifierCode(), "Entity Identifier Code should match N101");
+        assertEquals(planSponsorName, segment.getPlanSponsorName(), "Plan Sponsor Name should match N102");
+        assertEquals(identificationCodeQualifier, segment.getIdentificationCodeQualifier(), "Identification Code Qualifier should match N103");
+        assertEquals(sponsorIdentifier, segment.getSponsorIdentifier(), "Sponsor Identifier should match N104");
+    }
+
+    @Test
+    public void testSettingSpecNamesGettingSpecNames() throws ValidationException {
+        Payer segment = new Payer.Builder()
+                .setEntityIdentifierCode(entityIdentifierCode)
+                .setPlanSponsorName(planSponsorName)
+                .setIdentificationCodeQualifier(identificationCodeQualifier)
+                .setSponsorIdentifier(sponsorIdentifier)
+                .build();
+
+        assertEquals(entityIdentifierCode, segment.getN101(), "Entity Identifier Code should match N101");
+        assertEquals(planSponsorName, segment.getN102(), "Plan Sponsor Name should match N102");
+        assertEquals(identificationCodeQualifier, segment.getN103(), "Identification Code Qualifier should match N103");
+        assertEquals(sponsorIdentifier, segment.getN104(), "Sponsor Identifier should match N104");
+    }
+}
