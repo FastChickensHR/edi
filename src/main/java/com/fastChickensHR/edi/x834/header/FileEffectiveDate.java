f584d186f0886a69df3cccaf8226b3e49cdb8e59
mandymoorefan <markbeeb@gmail.com>
2025-03-19 09:33:08 -0700

    feat(dtp-segment): create dtp segment and refactor existing date segment to extend this class

--- src/main/java/com/fastChickensHR/edi/x834/header/FileEffectiveDate.java
+++ src/main/java/com/fastChickensHR/edi/x834/header/FileEffectiveDate.java
@@ -7,84 +7,41 @@
  */
 package com.fastChickensHR.edi.x834.header;
 
-import com.fastChickensHR.edi.x834.common.Segment;
+import com.fastChickensHR.edi.x834.common.DtpSegment;
 import com.fastChickensHR.edi.x834.common.exception.ValidationException;
 import com.fastChickensHR.edi.x834.common.x834Context;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.Accessors;
 
-@Getter
-public abstract class FileEffectiveDate extends Segment {
-    public static final String SEGMENT_ID = "DTP";
+public class FileEffectiveDate extends DtpSegment {
     public static final String DEFAULT_DATE_TIME_QUALIFIER = "007";
 
-    private final String dtp01;
-    private final String dtp02;
-    private final String dtp03;
-
-    protected FileEffectiveDate(FileEffectiveDate.Builder builder) throws ValidationException {
-        this.dtp01 = builder.dtp01;
-        this.dtp02 = builder.dtp02;
-        this.dtp03 = builder.dtp03;
+    private FileEffectiveDate(FileEffectiveDate.Builder builder) throws ValidationException {
+        super(builder);
     }
 
-    @Override
-    public String getSegmentIdentifier() {
-        return SEGMENT_ID;
+    public static FileEffectiveDate.Builder builder(x834Context context) {
+        return new FileEffectiveDate.Builder(context);
     }
 
-    @Override
-    public String[] getElementValues() {
-        return new String[]{dtp01, dtp02, dtp03};
-    }
-
-    // Domain-specific accessor methods
-    public String getDateTimeQualifier() {
-        return getDtp01();
-    }
-
-    public String getDateTimeFormat() {
-        return getDtp02();
-    }
-
-    public String getDateTimePeriod() {
-        return getDtp03();
-    }
-
-    @Setter
     @Accessors(chain = true)
-    public static class Builder {
-        private String dtp01 = DEFAULT_DATE_TIME_QUALIFIER;
-        private String dtp02;
-        private String dtp03;
+    public static class Builder extends DtpSegment.AbstractBuilder<Builder> {
 
         public Builder(x834Context context) {
-            this.dtp02 = context.getDateFormat().getFormat();
-            this.dtp03 = context.getFormattedDocumentDate();
+            super(context);
+            this.dtp01 = DEFAULT_DATE_TIME_QUALIFIER;
         }
 
-        public Builder setDateTimeQualifier(String value) {
-            return setDtp01(value);
+        @Override
+        protected Builder self() {
+            return this;
         }
 
-        public Builder setDateTimeFormat(String value) {
-            return setDtp02(value);
-        }
-
-        public Builder setDateTimePeriod(String value) {
-            return setDtp03(value);
-        }
-
+        @Override
         public FileEffectiveDate build() throws ValidationException {
-            return new FileEffectiveDate.FileEffectiveDateImpl(this);
-        }
-    }
-
-    // Concrete implementation class that can be instantiated
-    private static class FileEffectiveDateImpl extends FileEffectiveDate {
-        private FileEffectiveDateImpl(Builder builder) throws ValidationException {
-            super(builder);
+            if (dtp03 == null || dtp03.isEmpty()) {
+                throw new ValidationException("dtp03 (Date Time Period) is required");
+            }
+            return new FileEffectiveDate(this);
         }
     }
 }
---
+++ src/test/java/com/fastChickensHR/edi/x834/common/DtpSegmentTest.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2025 FastChickensHR <contact@fastchickenshr.com>
+ *
+ * This file is part of the FastChickensHR project.
+ *
+ * For license information see the LICENSE file in the root of this project.
+ */
+package com.fastChickensHR.edi.x834.common;
+
+import com.fastChickensHR.edi.x834.common.dates.DateFormat;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DtpSegmentTest {
+    x834Context context = new x834Context();
+
+    private static class TestDtpSegment extends DtpSegment {
+        public static final String TEST_QUALIFIER = "123";
+
+        private TestDtpSegment(AbstractBuilder<?> builder) throws ValidationException {
+            super(builder);
+        }
+
+        public static class Builder extends AbstractBuilder<Builder> {
+            public Builder(x834Context context) {
+                super(context);
+                this.dtp01 = TEST_QUALIFIER;
+            }
+
+            @Override
+            protected Builder self() {
+                return this;
+            }
+
+            @Override
+            public TestDtpSegment build() throws ValidationException {
+                if (dtp01 == null || dtp01.isEmpty()) {
+                    throw new ValidationException("dtp01 (Date Time Qualifier) is required");
+                }
+                if (dtp03 == null || dtp03.isEmpty()) {
+                    throw new ValidationException("dtp03 (Date Time Period) is required");
+                }
+                return new TestDtpSegment(this);
+            }
+        }
+    }
+
+    @Test
+    void testSegmentIdentifier() throws ValidationException {
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDtp02("D8")
+                .setDtp03("20250101")
+                .build();
+
+        assertEquals("DTP", segment.getSegmentIdentifier());
+    }
+
+    @Test
+    void testGetElementValues() throws ValidationException {
+        String qualifier = "456";
+        String format = "D8";
+        String date = "20250101";
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDtp01(qualifier)
+                .setDtp02(format)
+                .setDtp03(date)
+                .build();
+
+        String[] elements = segment.getElementValues();
+        assertEquals(3, elements.length);
+        assertEquals(qualifier, elements[0]);
+        assertEquals(format, elements[1]);
+        assertEquals(date, elements[2]);
+    }
+
+    @Test
+    void testDomainGetters() throws ValidationException {
+        String qualifier = "456";
+        String format = "D8";
+        String date = "20250101";
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDtp01(qualifier)
+                .setDtp02(format)
+                .setDtp03(date)
+                .build();
+
+        assertEquals(qualifier, segment.getDateTimeQualifier());
+        assertEquals(format, segment.getDateTimeFormat());
+        assertEquals(date, segment.getDateTimePeriod());
+
+        assertEquals(qualifier, segment.getDtp01());
+        assertEquals(format, segment.getDtp02());
+        assertEquals(date, segment.getDtp03());
+    }
+
+    @Test
+    void testBuilderWithSpecNamesSetters() throws ValidationException {
+        String qualifier = "789";
+        String format = "RD8";
+        String date = "20251231";
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDtp01(qualifier)
+                .setDtp02(format)
+                .setDtp03(date)
+                .build();
+
+        assertEquals(qualifier, segment.getDtp01());
+        assertEquals(format, segment.getDtp02());
+        assertEquals(date, segment.getDtp03());
+    }
+
+    @Test
+    void testBuilderWithDomainNameSetters() throws ValidationException {
+        String qualifier = "789";
+        String format = "RD8";
+        String date = "20251231";
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDateTimeQualifier(qualifier)
+                .setDateTimeFormat(format)
+                .setDateTimePeriod(date)
+                .build();
+
+        assertEquals(qualifier, segment.getDtp01());
+        assertEquals(format, segment.getDtp02());
+        assertEquals(date, segment.getDtp03());
+    }
+
+    @Test
+    void testRender() throws ValidationException {
+        String qualifier = "789";
+        String format = "D8";
+        String date = "20250615";
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .setDtp01(qualifier)
+                .setDtp02(format)
+                .setDtp03(date)
+                .build();
+        segment.setContext(context);
+
+        String rendered = segment.render();
+        assertEquals("DTP*789*D8*20250615~", rendered.trim());
+    }
+
+    @Test
+    void testContextBasedDefaults() throws ValidationException {
+        // Create a mock context with predefined date format and document date
+        x834Context context = new x834Context();
+        context.setDateFormat(DateFormat.CENTURY_YEAR);
+        context.setDocumentDate(LocalDate.of(2025, 3, 30));
+
+        TestDtpSegment segment = new TestDtpSegment.Builder(context)
+                .build();
+
+        assertEquals("123", segment.getDtp01()); // Default from TestDtpSegment
+        assertEquals("CCYY", segment.getDtp02()); // From context date format
+        assertEquals("2025", segment.getDtp03()); // From context document date
+    }
+
+    @Test
+    void testValidationRequiresDtp01AndDtp03() {
+        // Test missing dtp01
+        ValidationException exception1 = assertThrows(ValidationException.class, () -> {
+            new TestDtpSegment.Builder(context)
+                    .setDtp01("") // Empty value
+                    .setDtp02("D8")
+                    .setDtp03("20250101")
+                    .build();
+        });
+        assertTrue(exception1.getMessage().contains("dtp01"));
+
+        // Test missing dtp03
+        ValidationException exception2 = assertThrows(ValidationException.class, () -> {
+            new TestDtpSegment.Builder(context)
+                    .setDtp01("123")
+                    .setDtp02("D8")
+                    .setDtp03("") // Empty value
+                    .build();
+        });
+        assertTrue(exception2.getMessage().contains("dtp03"));
+    }
+}
