9bff2ec58476b41a7410649d4ae2cf2fcb9a76d0
mandymoorefan <markbeeb@gmail.com>
2025-03-20 14:03:39 -0700

    fix(loop2100c-member-mailing-address): created and added tests

--- src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberMailingAddress.java
+++ src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberMailingAddress.java
@@ -7,5 +7,73 @@
  */
 package com.fastChickensHR.edi.x834.loop2000.loop2100C;
 
-public class MemberMailingAddress {
+import com.fastChickensHR.edi.x834.common.NM1Segment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+
+/**
+ * Represents a Member Residence Street Address segment in X834 EDI format
+ * Currently implementing only the 01 and 02 elements
+ */
+@Getter
+public class MemberMailingAddress extends NM1Segment {
+
+    /**
+     * Entity Identifier Code (NM101) values for Member Residence Street Address
+     */
+    @Getter
+    public enum EntityIdentifierCode {
+        POSTAL_MAILING_ADDRESS("31", "Postal Mailing Address");
+
+        private final String code;
+        private final String description;
+
+        EntityIdentifierCode(String code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+    }
+
+    private MemberMailingAddress(Builder builder) throws ValidationException {
+        super(builder);
+    }
+
+    /**
+     * Builder for MemberResidenceStreetAddress
+     */
+    public static class Builder extends AbstractBuilder<Builder> {
+        public Builder() {
+            this.setNm101(EntityIdentifierCode.POSTAL_MAILING_ADDRESS.getCode());
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public MemberMailingAddress build() throws ValidationException {
+            return new MemberMailingAddress(this);
+        }
+
+        /**
+         * Sets the Entity Identifier Code (NM101) using the enum value
+         *
+         * @param value The Entity Identifier Code enum value
+         * @return this builder instance
+         */
+        public Builder setEntityIdentifierCode(EntityIdentifierCode value) {
+            return setNm101(value.getCode());
+        }
+    }
+
+    /**
+     * Creates a new Builder instance
+     *
+     * @return a new Builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
 }
