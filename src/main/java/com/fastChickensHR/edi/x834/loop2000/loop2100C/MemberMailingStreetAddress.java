6fb22e16d0b9f926dd4dc39af012f4f837b674aa
mandymoorefan <markbeeb@gmail.com>
2025-03-20 14:47:10 -0700

    feat(loop2100c-member-mailing-street): created and added tests

--- src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberMailingStreetAddress.java
+++ src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberMailingStreetAddress.java
@@ -7,5 +7,50 @@
  */
 package com.fastChickensHR.edi.x834.loop2000.loop2100C;
 
-public class MemberMailingStreetAddress {
+import com.fastChickensHR.edi.x834.common.N3Segment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+
+/**
+ * Represents the member's mailing street address in Loop 2100C.
+ * This extends the N3 segment to provide address information for a member.
+ */
+@Getter
+public class MemberMailingStreetAddress extends N3Segment {
+
+    private MemberMailingStreetAddress(Builder builder) throws ValidationException {
+        super(builder);
+        validate();
+    }
+
+    protected void validate() throws ValidationException {
+        if (n301 == null || n301.trim().isEmpty()) {
+            throw new ValidationException("Address Line 1 (N301) is required for Member Mailing Street Address");
+        }
+        // N302 is optional, no validation required
+    }
+
+    /**
+     * Builder for MemberMailingStreetAddress.
+     */
+    public static class Builder extends AbstractBuilder<Builder> {
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public MemberMailingStreetAddress build() throws ValidationException {
+            return new MemberMailingStreetAddress(this);
+        }
+    }
+
+    /**
+     * Creates a new Builder instance.
+     *
+     * @return a new Builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
 }
