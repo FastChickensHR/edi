593d1336e1692f5795b21668f3fc9f9f0d0ebbbc
mandymoorefan <markbeeb@gmail.com>
2025-03-20 14:35:35 -0700

    feat(loop2100a-member-demographics): created and added tests

--- src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberDemographics.java
+++ src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberDemographics.java
@@ -7,5 +7,50 @@
  */
 package com.fastChickensHR.edi.x834.loop2000.loop2100A;
 
-public class MemberDemographics {
+import com.fastChickensHR.edi.x834.common.DMGSegment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+
+/**
+ * Represents Member Demographics information in X834 EDI format
+ */
+@Getter
+public class MemberDemographics extends DMGSegment {
+
+    private MemberDemographics(Builder builder) throws ValidationException {
+        super(builder);
+    }
+
+    public static class Builder extends AbstractBuilder<Builder> {
+
+        protected void validate() throws ValidationException {
+            // At minimum, the format qualifier and birth date are required
+            if (dmg01 == null || dmg01.isEmpty()) {
+                throw new ValidationException("Date Time Period Format Qualifier (DMG01) is required");
+            }
+            if (dmg02 == null || dmg02.isEmpty()) {
+                throw new ValidationException("Birth Date (DMG02) is required");
+            }
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public MemberDemographics build() throws ValidationException {
+            validate();
+            return new MemberDemographics(this);
+        }
+    }
+
+    /**
+     * Creates a new Builder instance
+     *
+     * @return a new Builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
 }
---
+++ src/test/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberDemographicsTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2025 FastChickensHR <contact@fastchickenshr.com>
+ *
+ * This file is part of the FastChickensHR project.
+ *
+ * For license information see the LICENSE file in the root of this project.
+ */
+package com.fastChickensHR.edi.x834.loop2000.loop2100A;
+
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberDemographicsTest {
+
+    @Test
+    void testCreateWithRequiredFields() throws ValidationException {
+        String formatQualifier = "D8";
+        String birthDate = "19850315";
+
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDateTimePeriodFormatQualifier(formatQualifier)
+                .setBirthDate(birthDate)
+                .build();
+
+        assertEquals(formatQualifier, demographics.getDateTimePeriodFormatQualifier());
+        assertEquals(birthDate, demographics.getBirthDate());
+        assertNull(demographics.getGenderCode());
+    }
+
+    @Test
+    void testCreateWithGender() throws ValidationException {
+        String formatQualifier = "D8";
+        String birthDate = "19850315";
+        String genderCode = "F";  // Female
+
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDateTimePeriodFormatQualifier(formatQualifier)
+                .setBirthDate(birthDate)
+                .setGenderCode(genderCode)
+                .build();
+
+        assertEquals(formatQualifier, demographics.getDateTimePeriodFormatQualifier());
+        assertEquals(birthDate, demographics.getBirthDate());
+        assertEquals(genderCode, demographics.getGenderCode());
+    }
+
+    @Test
+    void testCreateWithAllDemographics() throws ValidationException {
+        String formatQualifier = "D8";
+        String birthDate = "19850315";
+        String genderCode = "F";
+        String maritalStatus = "S";
+        String raceCode = "7";
+
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDateTimePeriodFormatQualifier(formatQualifier)
+                .setBirthDate(birthDate)
+                .setGenderCode(genderCode)
+                .setMaritalStatusCode(maritalStatus)
+                .setRaceOrEthnicityCode(raceCode)
+                .build();
+
+        assertEquals(formatQualifier, demographics.getDateTimePeriodFormatQualifier());
+        assertEquals(birthDate, demographics.getBirthDate());
+        assertEquals(genderCode, demographics.getGenderCode());
+        assertEquals(maritalStatus, demographics.getMaritalStatusCode());
+        assertEquals(raceCode, demographics.getRaceOrEthnicityCode());
+    }
+
+    @Test
+    void testCreateWithDirectDMGElementSetters() throws ValidationException {
+        String formatQualifier = "D8";
+        String birthDate = "19850315";
+        String genderCode = "M";
+
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDmg01(formatQualifier)
+                .setDmg02(birthDate)
+                .setDmg03(genderCode)
+                .build();
+
+        assertEquals(formatQualifier, demographics.getDateTimePeriodFormatQualifier());
+        assertEquals(birthDate, demographics.getBirthDate());
+        assertEquals(genderCode, demographics.getGenderCode());
+    }
+
+    @Test
+    void testBuildWithoutFormatQualifierShouldFail() {
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> MemberDemographics.builder()
+                        .setBirthDate("19850315")
+                        .build()
+        );
+
+        assertTrue(exception.getMessage().contains("DMG01") ||
+                exception.getMessage().contains("Format Qualifier"));
+    }
+
+    @Test
+    void testBuildWithoutBirthDateShouldFail() {
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> MemberDemographics.builder()
+                        .setDateTimePeriodFormatQualifier("D8")
+                        .build()
+        );
+
+        assertTrue(exception.getMessage().contains("DMG02") ||
+                exception.getMessage().contains("Birth Date"));
+    }
+
+    @Test
+    void testSegmentIdentifier() throws ValidationException {
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDateTimePeriodFormatQualifier("D8")
+                .setBirthDate("19850315")
+                .build();
+
+        assertEquals("DMG", demographics.getSegmentIdentifier());
+    }
+
+    @Test
+    void testElementValues() throws ValidationException {
+        String formatQualifier = "D8";
+        String birthDate = "19850315";
+        String genderCode = "M";
+
+        MemberDemographics demographics = MemberDemographics.builder()
+                .setDateTimePeriodFormatQualifier(formatQualifier)
+                .setBirthDate(birthDate)
+                .setGenderCode(genderCode)
+                .build();
+
+        String[] elements = demographics.getElementValues();
+
+        assertEquals(formatQualifier, elements[0]);
+        assertEquals(birthDate, elements[1]);
+        assertEquals(genderCode, elements[2]);
+    }
+}
