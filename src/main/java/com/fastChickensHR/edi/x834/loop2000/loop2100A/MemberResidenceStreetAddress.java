3f4723a246413999982ba21a2f04701fadf427ac
mandymoorefan <markbeeb@gmail.com>
2025-03-20 14:03:21 -0700

    fix(loop2100a-member-residence-street-address): created as an nm1Segment that really was the loop2000c segment and not the loop2000a.

--- src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberResidenceStreetAddress.java
+++ src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberResidenceStreetAddress.java
@@ -7,33 +7,16 @@
  */
 package com.fastChickensHR.edi.x834.loop2000.loop2100A;
 
-import com.fastChickensHR.edi.x834.common.NM1Segment;
+import com.fastChickensHR.edi.x834.common.N3Segment;
 import com.fastChickensHR.edi.x834.common.exception.ValidationException;
 import lombok.Getter;
 
 /**
  * Represents a Member Residence Street Address segment in X834 EDI format
- * Currently implementing only the 01 and 02 elements
+ * Implements address line 1 (N301) and address line 2 (N302)
  */
 @Getter
-public class MemberResidenceStreetAddress extends NM1Segment {
-
-    /**
-     * Entity Identifier Code (NM101) values for Member Residence Street Address
-     */
-    @Getter
-    public enum EntityIdentifierCode {
-        POSTAL_MAILING_ADDRESS("31", "Postal Mailing Address");
-
-        private final String code;
-        private final String description;
-
-        EntityIdentifierCode(String code, String description) {
-            this.code = code;
-            this.description = description;
-        }
-
-    }
+public class MemberResidenceStreetAddress extends N3Segment {
 
     private MemberResidenceStreetAddress(Builder builder) throws ValidationException {
         super(builder);
@@ -43,10 +26,6 @@ public class MemberResidenceStreetAddress extends NM1Segment {
      * Builder for MemberResidenceStreetAddress
      */
     public static class Builder extends AbstractBuilder<Builder> {
-        public Builder() {
-            this.setNm101(EntityIdentifierCode.POSTAL_MAILING_ADDRESS.getCode());
-        }
-
         @Override
         protected Builder self() {
             return this;
@@ -55,16 +34,6 @@ public class MemberResidenceStreetAddress extends NM1Segment {
         @Override
         public MemberResidenceStreetAddress build() throws ValidationException {
             return new MemberResidenceStreetAddress(this);
-        }
-
-        /**
-         * Sets the Entity Identifier Code (NM101) using the enum value
-         *
-         * @param value The Entity Identifier Code enum value
-         * @return this builder instance
-         */
-        public Builder setEntityIdentifierCode(EntityIdentifierCode value) {
-            return setNm101(value.getCode());
         }
     }
 
--- src/test/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberResidenceStreetAddressTest.java
+++ src/test/java/com/fastChickensHR/edi/x834/loop2000/loop2100A/MemberResidenceStreetAddressTest.java
@@ -14,26 +14,71 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberResidenceStreetAddressTest {
 
     @Test
-    void testDefaultEntityIdentifierCode() throws ValidationException {
+    void testCreateWithAddressLine1Only() throws ValidationException {
+        String addressLine1 = "123 Main Street";
+
         MemberResidenceStreetAddress address = MemberResidenceStreetAddress.builder()
+                .setAddressLine1(addressLine1)
                 .build();
 
-        assertEquals("31", address.getNm101());
-        assertEquals("31", address.getEntityIdentifierCode());
+        assertEquals(addressLine1, address.getAddressLine1());
+        assertEquals(addressLine1, address.getN301());
+        assertNull(address.getAddressLine2());
+        assertNull(address.getN302());
     }
 
     @Test
-    void testEntityIdentifierCodeEnum() {
-        assertEquals("31", MemberResidenceStreetAddress.EntityIdentifierCode.POSTAL_MAILING_ADDRESS.getCode());
-        assertEquals("Postal Mailing Address", MemberResidenceStreetAddress.EntityIdentifierCode.POSTAL_MAILING_ADDRESS.getDescription());
+    void testCreateWithBothAddressLines() throws ValidationException {
+        String addressLine1 = "123 Main Street";
+        String addressLine2 = "Apt 4B";
+
+        MemberResidenceStreetAddress address = MemberResidenceStreetAddress.builder()
+                .setAddressLine1(addressLine1)
+                .setAddressLine2(addressLine2)
+                .build();
+
+        assertEquals(addressLine1, address.getAddressLine1());
+        assertEquals(addressLine2, address.getAddressLine2());
+        assertEquals(addressLine1, address.getN301());
+        assertEquals(addressLine2, address.getN302());
     }
 
     @Test
-    void testSetEntityIdentifierCodeUsingEnum() throws ValidationException {
+    void testCreateWithDirectN3ElementSetters() throws ValidationException {
+        String addressLine1 = "123 Main Street";
+        String addressLine2 = "Apt 4B";
+
         MemberResidenceStreetAddress address = MemberResidenceStreetAddress.builder()
-                .setEntityIdentifierCode(MemberResidenceStreetAddress.EntityIdentifierCode.POSTAL_MAILING_ADDRESS)
+                .setN301(addressLine1)
+                .setN302(addressLine2)
                 .build();
 
-        assertEquals("31", address.getNm101());
+        assertEquals(addressLine1, address.getAddressLine1());
+        assertEquals(addressLine2, address.getAddressLine2());
+    }
+
+    @Test
+    void testSegmentIdentifier() throws ValidationException {
+        MemberResidenceStreetAddress address = MemberResidenceStreetAddress.builder()
+                .setAddressLine1("123 Main Street")
+                .build();
+
+        assertEquals("N3", address.getSegmentIdentifier());
+    }
+
+    @Test
+    void testElementValues() throws ValidationException {
+        String addressLine1 = "123 Main Street";
+        String addressLine2 = "Apt 4B";
+
+        MemberResidenceStreetAddress address = MemberResidenceStreetAddress.builder()
+                .setAddressLine1(addressLine1)
+                .setAddressLine2(addressLine2)
+                .build();
+
+        String[] elements = address.getElementValues();
+        assertEquals(addressLine1, elements[0]);
+        assertEquals(addressLine2, elements[1]);
+        assertEquals(2, elements.length);
     }
 }
