b4fc2109bfd9fa74aed3a3e6de11b61912d6c51f
mandymoorefan <markbeeb@gmail.com>
2025-03-20 14:08:30 -0700

    fix(loop2100a-member-residence-city-state-zip-code): created and added tests

--- src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberResidenceCityStateZipCode.java
+++ src/main/java/com/fastChickensHR/edi/x834/loop2000/loop2100C/MemberResidenceCityStateZipCode.java
@@ -7,5 +7,42 @@
  */
 package com.fastChickensHR.edi.x834.loop2000.loop2100C;
 
-public class MemberResidenceCityStateZipCode {
+import com.fastChickensHR.edi.x834.common.N4Segment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+
+/**
+ * Represents a Member Residence City, State, and Zip Code segment in X834 EDI format
+ * Implements city name (N401), state/province code (N402), and postal code (N403)
+ */
+@Getter
+public class MemberResidenceCityStateZipCode extends N4Segment {
+
+    private MemberResidenceCityStateZipCode(Builder builder) throws ValidationException {
+        super(builder);
+    }
+
+    /**
+     * Builder for MemberResidenceCityStateZipCode
+     */
+    public static class Builder extends AbstractBuilder<Builder> {
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public MemberResidenceCityStateZipCode build() throws ValidationException {
+            return new MemberResidenceCityStateZipCode(this);
+        }
+    }
+
+    /**
+     * Creates a new Builder instance
+     *
+     * @return a new Builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
 }
