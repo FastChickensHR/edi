261b3673eb7f36a23c9c91ff9d78c5d849efdc2a
mandymoorefan <markbeeb@gmail.com>
2025-03-22 12:35:51 -0700

    feat(transaction-set-trailer): created and added tests

---
+++ src/main/java/com/fastChickensHR/edi/x834/trailer/TransactionSetTrailer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2025 FastChickensHR <contact@fastchickenshr.com>
+ *
+ * This file is part of the FastChickensHR project.
+ *
+ * For license information see the LICENSE file in the root of this project.
+ */
+package com.fastChickensHR.edi.x834.trailer;
+
+import com.fastChickensHR.edi.x834.common.SESegment;
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * Represents the Transaction Set Trailer in an EDI 834 transaction.
+ * This class extends the SESegment (SE - Transaction Set Trailer segment).
+ */
+@Getter
+public class TransactionSetTrailer extends SESegment {
+
+    private TransactionSetTrailer(Builder builder) throws ValidationException {
+        super(builder);
+    }
+
+    /**
+     * Creates a new Builder instance for TransactionSetTrailer.
+     *
+     * @return a new Builder instance
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for TransactionSetTrailer.
+     */
+    @Setter
+    @Accessors(chain = true)
+    public static class Builder extends AbstractBuilder<Builder> {
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public TransactionSetTrailer build() throws ValidationException {
+            return new TransactionSetTrailer(this);
+        }
+    }
+}
---
+++ src/test/java/com/fastChickensHR/edi/x834/trailer/TransactionSetTrailerTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2025 FastChickensHR <contact@fastchickenshr.com>
+ *
+ * This file is part of the FastChickensHR project.
+ *
+ * For license information see the LICENSE file in the root of this project.
+ */
+package com.fastChickensHR.edi.x834.trailer;
+
+import com.fastChickensHR.edi.x834.common.exception.ValidationException;
+import com.fastChickensHR.edi.x834.common.x834Context;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionSetTrailerTest {
+
+    @Test
+    void testInheritance() {
+        TransactionSetTrailer trailer = null;
+        try {
+            trailer = TransactionSetTrailer.builder()
+                    .setTransactionSegmentCount("10")
+                    .setSetControlNumber("1234")
+                    .build();
+        } catch (ValidationException e) {
+            fail("Failed to build TransactionSetTrailer: " + e.getMessage());
+        }
+
+        assertNotNull(trailer);
+    }
+
+    @Test
+    void testBuilderCreation() {
+        TransactionSetTrailer.Builder builder = TransactionSetTrailer.builder();
+        assertNotNull(builder);
+    }
+
+    @Test
+    void testUsageInTransactionContext() throws ValidationException {
+        TransactionSetTrailer trailer = TransactionSetTrailer.builder()
+                .setTransactionSegmentCount("25")
+                .setSetControlNumber("9876")
+                .build();
+
+        assertEquals("25", trailer.getTransactionSegmentCount());
+        assertEquals("9876", trailer.getSetControlNumber());
+        assertEquals("SE", trailer.getSegmentIdentifier());
+
+        String[] elements = trailer.getElementValues();
+        assertEquals(2, elements.length);
+        assertEquals("25", elements[0]);
+        assertEquals("9876", elements[1]);
+    }
+
+    @Test
+    void testToString() throws ValidationException {
+        TransactionSetTrailer trailer = TransactionSetTrailer.builder()
+                .setTransactionSegmentCount("15")
+                .setSetControlNumber("5678")
+                .build();
+        trailer.setContext(new x834Context());
+
+        String segment = trailer.render().trim();
+
+        assertEquals("SE*15*5678~", segment);
+    }
+}
