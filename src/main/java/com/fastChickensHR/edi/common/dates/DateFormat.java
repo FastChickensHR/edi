7d1e257530249d7cf6a4a18e8ad5be7432b911f5
mandymoorefan <markbeeb@gmail.com>
2025-04-14 08:20:17 -0700

    chore(date-format): add additional date formats

--- src/main/java/com/fastChickensHR/edi/common/dates/DateFormat.java
+++ src/main/java/com/fastChickensHR/edi/common/dates/DateFormat.java
@@ -32,7 +32,212 @@ public enum DateFormat {
     /**
      * Month and day format MMDD (e.g., 1115)
      */
-    MONTH_DAY("MMDD", "^\\d{4}$");
+    MONTH_DAY("MMDD", "^\\d{4}$"),
+
+    /**
+     * First two digits of year (e.g., 20)
+     */
+    CC("CC", "^\\d{2}$"),
+
+    /**
+     * Month and year in format MMMYYYY (e.g., JAN1994)
+     */
+    CD("CD", "^[A-Z]{3}\\d{4}$"),
+
+    /**
+     * Date in format YYYYMM (e.g., 202311)
+     */
+    CM("CM", "^\\d{6}$"),
+
+    /**
+     * Date in format YYYYQ (year and quarter) (e.g., 2023Q)
+     */
+    CQ("CQ", "^\\d{4}[1-4Q]$"),
+
+    /**
+     * Year in format YYYY (e.g., 2023)
+     */
+    CY("CY", "^\\d{4}$"),
+
+    /**
+     * Date in format YYMMDD (e.g., 231115)
+     */
+    D6("D6", "^\\d{6}$"),
+
+    /**
+     * Range of days within a month in format DD-DD (e.g., 01-15)
+     */
+    DA("DA", "^\\d{2}-\\d{2}$"),
+
+    /**
+     * Date in format MMDDYYYY (e.g., 11152023)
+     */
+    DB("DB", "^\\d{8}$"),
+
+    /**
+     * Day of month with leading zero (e.g., 01-31)
+     */
+    DD("DD", "^\\d{2}$"),
+
+    /**
+     * Range of dates and time in format YYYYMMDD-YYYYMMDDHHMM
+     */
+    DDT("DDT", "^\\d{8}-\\d{12}$"),
+
+    /**
+     * Date and time in format YYYYMMDDHHMM (e.g., 202311151430)
+     */
+    DT("DT", "^\\d{12}$"),
+
+    /**
+     * Range of dates and time in format YYYYMMDDHHMM-YYYYMMDD
+     */
+    DTD("DTD", "^\\d{12}-\\d{8}$"),
+
+    /**
+     * Range of date and time in format YYYYMMDDHHMMSS-YYYYMMDDHHMMSS
+     */
+    DTS("DTS", "^\\d{14}-\\d{14}$"),
+
+    /**
+     * Last digit of year and Julian date in format YDDD (e.g., 3319)
+     */
+    EH("EH", "^\\d{4}$"),
+
+    /**
+     * Date in format YYMMMDD (e.g., 23NOV15)
+     */
+    KA("KA", "^\\d{2}[A-Z]{3}\\d{2}$"),
+
+    /**
+     * Date in format MMYYYY (e.g., 112023)
+     */
+    MCY("MCY", "^\\d{6}$"),
+
+    /**
+     * Month and day in format MMDD (e.g., 1115)
+     */
+    MD("MD", "^\\d{4}$"),
+
+    /**
+     * Month with leading zero (e.g., 01-12)
+     */
+    MM("MM", "^\\d{2}$"),
+
+    /**
+     * Range of dates in format MMDDYYYY-MMDDYYYY
+     */
+    RD("RD", "^\\d{8}-\\d{8}$"),
+
+    /**
+     * Range of years in format YY-YY (e.g., 22-23)
+     */
+    RD2("RD2", "^\\d{2}-\\d{2}$"),
+
+    /**
+     * Range of years in format YYYY-YYYY (e.g., 2022-2023)
+     */
+    RD4("RD4", "^\\d{4}-\\d{4}$"),
+
+    /**
+     * Range of years and months in format YYYYMM-YYYYMM
+     */
+    RD5("RD5", "^\\d{6}-\\d{6}$"),
+
+    /**
+     * Range of dates in format YYMMDD-YYMMDD
+     */
+    RD6("RD6", "^\\d{6}-\\d{6}$"),
+
+    /**
+     * Range of dates in format YYYYMMDD-YYYYMMDD
+     */
+    RD8("RD8", "^\\d{8}-\\d{8}$"),
+
+    /**
+     * Range of dates in format YYMMDD-MMDD
+     */
+    RDM("RDM", "^\\d{6}-\\d{4}$"),
+
+    /**
+     * Range of date and time in format YYYYMMDDHHMM-YYYYMMDDHHMM
+     */
+    RDT("RDT", "^\\d{12}-\\d{12}$"),
+
+    /**
+     * Range of months and days in format MMDD-MMDD
+     */
+    RMD("RMD", "^\\d{4}-\\d{4}$"),
+
+    /**
+     * Range of years and months in format YYMM-YYMM
+     */
+    RMY("RMY", "^\\d{4}-\\d{4}$"),
+
+    /**
+     * Range of time in format HHMM-HHMM
+     */
+    RTM("RTM", "^\\d{4}-\\d{4}$"),
+
+    /**
+     * Date and time in format YYYYMMDDHHMMSS
+     */
+    RTS("RTS", "^\\d{14}$"),
+
+    /**
+     * Julian date in format DDD (e.g., 319)
+     */
+    TC("TC", "^\\d{3}$"),
+
+    /**
+     * Time in format HHMM (e.g., 1430)
+     */
+    TM("TM", "^\\d{4}$"),
+
+    /**
+     * Date in format MMYY (e.g., 1123)
+     */
+    TQ("TQ", "^\\d{4}$"),
+
+    /**
+     * Date and time in format DDMMYYHHMM
+     */
+    TR("TR", "^\\d{10}$"),
+
+    /**
+     * Time in format HHMMSS (e.g., 143015)
+     */
+    TS("TS", "^\\d{6}$"),
+
+    /**
+     * Date in format MMDDYY (e.g., 111523)
+     */
+    TT("TT", "^\\d{6}$"),
+
+    /**
+     * Date in format YYDDD (Julian date, e.g., 23319)
+     */
+    TU("TU", "^\\d{5}$"),
+
+    /**
+     * Unstructured date format
+     */
+    UN("UN", ".*"),
+
+    /**
+     * Year and month in format YYMM (e.g., 2311)
+     */
+    YM("YM", "^\\d{4}$"),
+
+    /**
+     * Range of year and months in format YYYYMMM-MMM (e.g., 2023JAN-DEC)
+     */
+    YMM("YMM", "^\\d{4}[A-Z]{3}-[A-Z]{3}$"),
+
+    /**
+     * Last two digits of year (e.g., 23)
+     */
+    YY("YY", "^\\d{2}$");
 
     private final String format;
     private final String validationPattern;
@@ -41,5 +246,4 @@ public enum DateFormat {
         this.format = format;
         this.validationPattern = validationPattern;
     }
-
 }
--- src/main/java/com/fastChickensHR/edi/common/dates/DateFormatter.java
+++ src/main/java/com/fastChickensHR/edi/common/dates/DateFormatter.java
@@ -26,6 +26,47 @@ public class DateFormatter {
             case DATE, D8 -> date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
             case CENTURY_YEAR -> Integer.toString(date.getYear());
             case MONTH_DAY -> date.format(DateTimeFormatter.ofPattern("MMdd"));
+            case CC -> date.format(DateTimeFormatter.ofPattern("yy"));
+            case CD -> date.format(DateTimeFormatter.ofPattern("MMMM"));  // Full month name
+            case CM, MM -> date.format(DateTimeFormatter.ofPattern("MM"));    // Month number
+            case CQ -> String.valueOf((date.getMonthValue() - 1) / 3 + 1); // Quarter (1-4)
+            case CY -> date.format(DateTimeFormatter.ofPattern("yyyy"));   // Four-digit year
+            case D6 -> date.format(DateTimeFormatter.ofPattern("yyMMdd")); // Six-digit date
+            case DA -> date.format(DateTimeFormatter.ofPattern("dd"));     // Day of month
+            case DB -> date.format(DateTimeFormatter.ofPattern("ddMMM"));  // Day and abbreviated month
+            case DD -> date.format(DateTimeFormatter.ofPattern("dd"));     // Day of month
+            case DDT -> date.format(DateTimeFormatter.ofPattern("dd'T'HHmm")); // Day + time
+            case DT -> date.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")); // ISO-like date+time
+            case DTD -> date.format(DateTimeFormatter.ofPattern("yyyyMMdd")); // Date in yyyyMMdd format
+            case DTS -> date.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")); // Date-time stamp
+            case EH -> date.format(DateTimeFormatter.ofPattern("HH"));     // Hour (24-hour format)
+            case KA -> date.format(DateTimeFormatter.ofPattern("HHmm"));   // Time in HHMM format
+            case MCY -> date.format(DateTimeFormatter.ofPattern("MMMM yyyy")); // Month name and year
+            case MD -> date.format(DateTimeFormatter.ofPattern("MMdd"));   // Month and day
+            case RD -> date.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")); // MM/DD/YYYY format
+            case RD2 -> date.format(DateTimeFormatter.ofPattern("MM-dd-yyyy")); // MM-DD-YYYY format
+            case RD4 -> date.format(DateTimeFormatter.ofPattern("MM.dd.yyyy")); // MM.DD.YYYY format
+            case RD5 -> date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); // YYYY-MM-DD format
+            case RD6 -> date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd")); // YYYY/MM/DD format
+            case RD8 -> date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")); // YYYY.MM.DD format
+            case RDM -> date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")); // DD/MM/YYYY format
+            case RDT -> date.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss")); // MM/DD/YYYY HH:MM:SS
+            case RMD -> date.format(DateTimeFormatter.ofPattern("M/d/yyyy")); // M/D/YYYY (no leading zeros)
+            case RMY -> date.format(DateTimeFormatter.ofPattern("MMM yyyy")); // Month abbreviation and year
+            case RTM -> date.format(DateTimeFormatter.ofPattern("HH:mm:ss")); // Time with colons
+            case RTS -> date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")); // ISO 8601
+            case TC -> date.format(DateTimeFormatter.ofPattern("HH"));     // Hour component
+            case TM -> date.format(DateTimeFormatter.ofPattern("mm"));     // Minute component
+            case TQ -> date.format(DateTimeFormatter.ofPattern("HHmm"));   // HHMM time format
+            case TR -> date.format(DateTimeFormatter.ofPattern("HH:mm"));  // HH:MM time format
+            case TS -> date.format(DateTimeFormatter.ofPattern("ss"));     // Second component
+            case TT -> date.format(DateTimeFormatter.ofPattern("HHmmss")); // HHMMSS time format
+            case TU -> date.format(DateTimeFormatter.ofPattern("HH:mm:ss")); // HH:MM:SS time format
+            case UN -> date.format(DateTimeFormatter.ofPattern("u"));      // Day of week (1=Monday, 7=Sunday)
+            case YM -> date.format(DateTimeFormatter.ofPattern("yyyyMM")); // Year and month
+            case YMM -> date.format(DateTimeFormatter.ofPattern("yyyy MMM")); // Year and month abbreviation
+            case YY -> date.format(DateTimeFormatter.ofPattern("yy"));     // Two-digit year
+
         };
     }
 
